e5532863af4eb4c9d2bd0f991e5a5930ed20b63c
Curtis McEnroe <programble@gmail.com>
2011-04-04 00:22:25 -0400

    Added an FPS counter

--- lib/window.rb
+++ lib/window.rb
@@ -11,6 +11,8 @@ class GameWindow < Gosu::Window
     
     @debug_font = Gosu::Font.new(self, Gosu::default_font_name, 20)
     
+    @framerate = 0.0
+    
     @ball = Ball.new(self, 10, 10)
     @ball.speed = 1.0
     @ball.angle = 0
@@ -23,11 +25,16 @@ class GameWindow < Gosu::Window
   end
 
   def draw
+    frame_start = Time.new
+    
     @debug_font.draw("(#{@ball.x.round},#{@ball.y.round}) #{@ball.angle} #{@ball.speed.round(2)}", 0, 0, ZOrder::DEBUG, 1.0, 1.0, Gosu::Color::GREEN)
+    @debug_font.draw(@framerate.ceil.to_s, 0, 20, ZOrder::DEBUG, 1.0, 1.0, Gosu::Color::GREEN)
     
     # Background
     draw_quad(0, 0, Gosu::Color::BLACK, width, 0, Gosu::Color::BLACK, 0, height, Gosu::Color::BLACK, width, height, Gosu::Color::BLACK, ZOrder::BACKGROUND)
     
     @ball.draw
+    
+    @framerate = 60 / (Time.new - frame_start) / 10000
   end
 end
