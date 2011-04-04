da7e8b77234a7bea7079b3c4295d5fd94f42f9d0
Curtis McEnroe <programble@gmail.com>
2011-04-03 23:20:57 -0400

    Moved code common to rectangles into a separate base class

--- lib/ball.rb
+++ lib/ball.rb
@@ -1,20 +1,15 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__)))
+require 'rectangle'
 require 'zorder'
 
 require 'gosu'
 
-class Ball
+class Ball < Rectangle
   attr_accessor :angle, :speed
   attr_reader :x, :y, :width, :height
 
   def initialize(window, width, height)
-    @window = window
-    @x = @y = @angle = @speed = 0
-    @width, @height = width, height
-  end
-  
-  def warp(x, y)
-    @x, @y = x, y
+    super(window, width, height, Gosu::Color::WHITE, ZOrder::BALL, true, ZOrder::BALL_GLOW)
   end
   
   def move
@@ -24,64 +19,6 @@ class Ball
     # Top/Bottom collision
     if Gosu::distance(@x, @y, @x, 0) <= @speed || Gosu::distance(@x, @y, @x, @window.height) <= @speed
       @angle = 180 - @angle
-    end
-  end
-  
-  def draw
-    @window.rotate(@angle, @x, @y) do
-      @window.translate(@x, @y) do
-        hw, hh = @width / 2.0, @height / 2.0
-        # Ball
-        @window.draw_quad(-hw, -hh, Gosu::Color::WHITE, 
-                           hw, -hh, Gosu::Color::WHITE, 
-                           hw,  hh, Gosu::Color::WHITE, 
-                          -hw,  hh, Gosu::Color::WHITE, 
-                          ZOrder::BALL)
-        # Top Glow
-        @window.draw_quad(-hw, -hh,      Gosu::Color::WHITE, 
-                          -hw, -@height, Gosu::Color::NONE, 
-                           hw, -@height, Gosu::Color::NONE, 
-                           hw, -hh,      Gosu::Color::WHITE, 
-                          ZOrder::BALL_GLOW)
-        # Right Glow
-        @window.draw_quad(hw,     -hh, Gosu::Color::WHITE, 
-                          @width, -hh, Gosu::Color::NONE, 
-                          @width,  hh, Gosu::Color::NONE, 
-                          hw,      hh, Gosu::Color::WHITE, 
-                          ZOrder::BALL_GLOW)
-        # Bottom Glow
-        @window.draw_quad(-hw, hh,      Gosu::Color::WHITE, 
-                           hw, hh,      Gosu::Color::WHITE, 
-                           hw, @height, Gosu::Color::NONE, 
-                          -hw, @height, Gosu::Color::NONE, 
-                          ZOrder::BALL_GLOW)
-        # Left Glow
-        @window.draw_quad(-hw,     -hh, Gosu::Color::WHITE, 
-                          -hw,      hh, Gosu::Color::WHITE, 
-                          -@width,  hh, Gosu::Color::NONE, 
-                          -@width, -hh, Gosu::Color::NONE, 
-                          ZOrder::BALL_GLOW)
-        # Top/Right Glow
-        @window.draw_triangle(hw,     -hh,      Gosu::Color::WHITE, 
-                              hw,     -@height, Gosu::Color::NONE, 
-                              @width, -hh,      Gosu::Color::NONE, 
-                              ZOrder::BALL_GLOW)
-        # Bottom/Right Glow
-        @window.draw_triangle(hw,     hh,      Gosu::Color::WHITE, 
-                              @width, hh,      Gosu::Color::NONE, 
-                              hw,     @height, Gosu::Color::NONE, 
-                              ZOrder::BALL_GLOW)
-        # Bottom/Left Glow
-        @window.draw_triangle(-hw,     hh,      Gosu::Color::WHITE, 
-                              -hw,     @height, Gosu::Color::NONE, 
-                              -@width, hh,      Gosu::Color::NONE, 
-                              ZOrder::BALL_GLOW)
-        # Top/Left Glow
-        @window.draw_triangle(-hw,     -hh,      Gosu::Color::WHITE, 
-                              -@width, -hh,      Gosu::Color::NONE, 
-                              -hw,     -@height, Gosu::Color::NONE, 
-                              ZOrder::BALL_GLOW)
-      end
     end
   end
 end
---
+++ lib/rectangle.rb
@@ -0,0 +1,80 @@
+require 'gosu'
+
+class Rectangle
+  attr_accessor :angle
+  attr_reader :x, :y, :width, :height
+  
+  def initialize(window, width, height, color, zorder, glow, glow_zorder)
+    @window = window
+    @width, @height = width, height
+    @color = color
+    @zorder = zorder
+    @glow = glow
+    @glow_zorder = glow_zorder
+    @x = @y = @angle = 0
+  end
+  
+  def warp(x, y)
+    @x, @y = x, y
+  end
+  
+  def draw
+    @window.rotate(@angle, @x, @y) do
+      @window.translate(@x, @y) do
+        hw, hh = @width / 2.0, @height / 2.0
+        # Rectangle
+        @window.draw_quad(-hw, -hh, @color, 
+                           hw, -hh, @color, 
+                           hw,  hh, @color, 
+                          -hw,  hh, @color, 
+                          @zorder)
+        if @glow
+          # Top Glow
+          @window.draw_quad(-hw, -hh,      @color, 
+                            -hw, -@height, Gosu::Color::NONE, 
+                             hw, -@height, Gosu::Color::NONE, 
+                             hw, -hh,      @color, 
+                            @glow_zorder)
+          # Right Glow
+          @window.draw_quad(hw,     -hh, @color, 
+                            @width, -hh, Gosu::Color::NONE, 
+                            @width,  hh, Gosu::Color::NONE, 
+                            hw,      hh, @color, 
+                            @glow_zorder)
+          # Bottom Glow
+          @window.draw_quad(-hw, hh,      @color, 
+                             hw, hh,      @color, 
+                             hw, @height, Gosu::Color::NONE, 
+                            -hw, @height, Gosu::Color::NONE, 
+                            @glow_zorder)
+          # Left Glow
+          @window.draw_quad(-hw,     -hh, @color, 
+                            -hw,      hh, @color, 
+                            -@width,  hh, Gosu::Color::NONE, 
+                            -@width, -hh, Gosu::Color::NONE, 
+                            @glow_zorder)
+          # Top/Right Glow
+          @window.draw_triangle(hw,     -hh,      @color, 
+                                hw,     -@height, Gosu::Color::NONE, 
+                                @width, -hh,      Gosu::Color::NONE, 
+                                @glow_zorder)
+          # Bottom/Right Glow
+          @window.draw_triangle(hw,     hh,      @color, 
+                                @width, hh,      Gosu::Color::NONE, 
+                                hw,     @height, Gosu::Color::NONE, 
+                                @glow_zorder)
+          # Bottom/Left Glow
+          @window.draw_triangle(-hw,     hh,      @color, 
+                                -hw,     @height, Gosu::Color::NONE, 
+                                -@width, hh,      Gosu::Color::NONE, 
+                                @glow_zorder)
+          # Top/Left Glow
+          @window.draw_triangle(-hw,     -hh,      @color, 
+                                -@width, -hh,      Gosu::Color::NONE, 
+                                -hw,     -@height, Gosu::Color::NONE, 
+                                @glow_zorder)
+        end
+      end
+    end
+  end
+end
