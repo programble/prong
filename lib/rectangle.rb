d2dbf86e149c5d3e30415891e987da6251dede59
Curtis McEnroe <programble@gmail.com>
2011-04-04 15:09:46 -0400

    Use a glow radius instead of the width/height of the rectangle

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -4,13 +4,14 @@ class Rectangle
   attr_accessor :angle, :color
   attr_reader :x, :y, :width, :height
   
-  def initialize(window, width, height, color, zorder, glow, glow_zorder)
+  def initialize(window, width, height, color, zorder, glow, glow_zorder=nil, glow_radius=5)
     @window = window
     @width, @height = width, height
     @color = color
     @zorder = zorder
     @glow = glow
     @glow_zorder = glow_zorder
+    @glow_radius = glow_radius
     @x = @y = @angle = 0
   end
   
@@ -30,48 +31,48 @@ class Rectangle
                           @zorder)
         if @glow
           # Top Glow
-          @window.draw_quad(-hw, -hh,      @color, 
-                            -hw, -@height, Gosu::Color::NONE, 
-                             hw, -@height, Gosu::Color::NONE, 
-                             hw, -hh,      @color, 
+          @window.draw_quad(-hw, -hh,                @color, 
+                            -hw, -hh - @glow_radius, Gosu::Color::NONE, 
+                             hw, -hh - @glow_radius, Gosu::Color::NONE, 
+                             hw, -hh,                @color, 
                             @glow_zorder)
           # Right Glow
-          @window.draw_quad(hw,     -hh, @color, 
-                            @width, -hh, Gosu::Color::NONE, 
-                            @width,  hh, Gosu::Color::NONE, 
-                            hw,      hh, @color, 
+          @window.draw_quad(hw,                -hh, @color, 
+                            hw + @glow_radius, -hh, Gosu::Color::NONE, 
+                            hw + @glow_radius,  hh, Gosu::Color::NONE, 
+                            hw,                 hh, @color, 
                             @glow_zorder)
           # Bottom Glow
-          @window.draw_quad(-hw, hh,      @color, 
-                             hw, hh,      @color, 
-                             hw, @height, Gosu::Color::NONE, 
-                            -hw, @height, Gosu::Color::NONE, 
+          @window.draw_quad(-hw, hh,                @color, 
+                             hw, hh,                @color, 
+                             hw, hh + @glow_radius, Gosu::Color::NONE, 
+                            -hw, hh + @glow_radius, Gosu::Color::NONE, 
                             @glow_zorder)
           # Left Glow
-          @window.draw_quad(-hw,     -hh, @color, 
-                            -hw,      hh, @color, 
-                            -@width,  hh, Gosu::Color::NONE, 
-                            -@width, -hh, Gosu::Color::NONE, 
+          @window.draw_quad(-hw,                -hh, @color, 
+                            -hw,                 hh, @color, 
+                            -hw - @glow_radius,  hh, Gosu::Color::NONE, 
+                            -hw - @glow_radius, -hh, Gosu::Color::NONE, 
                             @glow_zorder)
           # Top/Right Glow
-          @window.draw_triangle(hw,     -hh,      @color, 
-                                hw,     -@height, Gosu::Color::NONE, 
-                                @width, -hh,      Gosu::Color::NONE, 
+          @window.draw_triangle(hw,                -hh,                @color, 
+                                hw,                -hh - @glow_radius, Gosu::Color::NONE, 
+                                hw + @glow_radius, -hh,                Gosu::Color::NONE, 
                                 @glow_zorder)
           # Bottom/Right Glow
-          @window.draw_triangle(hw,     hh,      @color, 
-                                @width, hh,      Gosu::Color::NONE, 
-                                hw,     @height, Gosu::Color::NONE, 
+          @window.draw_triangle(hw,                hh,                @color, 
+                                hw + @glow_radius, hh,                Gosu::Color::NONE, 
+                                hw,                hh + @glow_radius, Gosu::Color::NONE, 
                                 @glow_zorder)
           # Bottom/Left Glow
-          @window.draw_triangle(-hw,     hh,      @color, 
-                                -hw,     @height, Gosu::Color::NONE, 
-                                -@width, hh,      Gosu::Color::NONE, 
+          @window.draw_triangle(-hw,                hh,                @color, 
+                                -hw,                hh + @glow_radius, Gosu::Color::NONE, 
+                                -hw - @glow_radius, hh,                Gosu::Color::NONE, 
                                 @glow_zorder)
           # Top/Left Glow
-          @window.draw_triangle(-hw,     -hh,      @color, 
-                                -@width, -hh,      Gosu::Color::NONE, 
-                                -hw,     -@height, Gosu::Color::NONE, 
+          @window.draw_triangle(-hw,                -hh,                @color, 
+                                -hw - @glow_radius, -hh,                Gosu::Color::NONE, 
+                                -hw,                -hh - @glow_radius, Gosu::Color::NONE, 
                                 @glow_zorder)
         end
       end
