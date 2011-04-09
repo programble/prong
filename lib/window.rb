1567e6df856faff6bdee280b78e4cd124e32d5bf
Curtis McEnroe <programble@gmail.com>
2011-04-08 20:16:50 -0400

    Made FPS and debug display toggleable through F2 and F3, and made escape close

--- lib/window.rb
+++ lib/window.rb
@@ -11,13 +11,16 @@ class GameWindow < Gosu::Window
     super(width, height, fullscreen)
     self.caption = "Prong"
     
-    @fps_font = Gosu::Font.new(self, Gosu::default_font_name, 15)
-    @score_font = Gosu::Font.new(self, File.join(File.dirname(__FILE__), '..', 'media', 'imagine_font.ttf'), 40)
-    @debug_font = Gosu::Font.new(self, Gosu::default_font_name, 20)
-    
     @frames = 0
     @framerate = 1
     @framerate_start = Time.new
+    
+    @show_fps = false
+    @debug = false
+    
+    @fps_font = Gosu::Font.new(self, Gosu::default_font_name, 15)
+    @score_font = Gosu::Font.new(self, File.join(File.dirname(__FILE__), '..', 'media', 'imagine_font.ttf'), 40)
+    @debug_font = Gosu::Font.new(self, Gosu::default_font_name, 20)
     
     @ball = Ball.new(self, 10, 10)
     @ball.speed = 2.0
@@ -38,17 +41,30 @@ class GameWindow < Gosu::Window
     @ai_paddle.calculate(@ball)
     @ai_paddle.move
   end
+  
+  def button_down(id)
+    case id
+    when Gosu::Button::KbEscape
+      close
+    when Gosu::Button::KbF2
+      @show_fps = !@show_fps
+    when Gosu::Button::KbF3
+      @debug = !@debug
+    end
+  end
 
   def draw
     @frames += 1
     
-    @debug_font.draw("(#{@ball.x.round},#{@ball.y.round}) angle: #{@ball.angle} speed: #{@ball.speed.round(2)}", 0, 0, ZOrder::DEBUG, 1.0, 1.0, Gosu::Color::GREEN)
+    @debug_font.draw("(#{@ball.x.round},#{@ball.y.round}) angle: #{@ball.angle} speed: #{@ball.speed.round(2)}", 0, 0, ZOrder::DEBUG, 1.0, 1.0, Gosu::Color::GREEN) if @debug
     
     # Background
     draw_quad(0, 0, Gosu::Color::BLACK, width, 0, Gosu::Color::BLACK, 0, height, Gosu::Color::BLACK, width, height, Gosu::Color::BLACK, ZOrder::BACKGROUND)
     
-    fps = "#{@framerate.round} FPS"
-    @fps_font.draw(fps, width - @fps_font.text_width(fps), 0, ZOrder::FPS, 1.0, 1.0, Gosu::Color::WHITE)
+    if @show_fps
+      fps = "#{@framerate.round} FPS"
+      @fps_font.draw(fps, width - @fps_font.text_width(fps), 0, ZOrder::FPS, 1.0, 1.0, Gosu::Color::WHITE)
+    end
     
     @score_font.draw(@player_paddle.score.to_s, width / 2 - 10 - @score_font.text_width(@player_paddle.score.to_s), 10, ZOrder::SCORE, 1.0, 1.0, Gosu::Color::WHITE)
     @score_font.draw(@ai_paddle.score.to_s, width / 2 + 10, 10, ZOrder::SCORE, 1.0, 1.0, Gosu::Color::WHITE)
